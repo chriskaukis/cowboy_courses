25832baa9527b208f11abc58a6d7d9fed587e9a9
Christopher Kaukis <christopher@interworks.com>
2015-05-09 16:46:56 -0500

    Added log out route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'log_in' => 'sessions#new', as: :log_in
   post 'log_in' => 'sessions#create'
+  get 'log_out' => 'sessions#destroy', as: :log_out
+  delete 'log_out' => 'sessions#destroy'
   get 'sign_up' => 'users#new', as: :sign_up
   post 'sign_up' => 'users#create'
 
